5ac4493b10a2d0a710bfaa490f101206015a8fa8
Suhas Reddy Mulkalla <suhasreddy.1993@gmail.com>
2019-02-10 17:55:00 +0530

    Add HTTP get api test file

---
+++ publicGetter.go
@@ -0,0 +1,25 @@
+package awesomeProject
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func GetPublicAPI() {
+
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	} else {
+		defer resp.Body.Close()
+		contents, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", string(contents))
+	}
+}
